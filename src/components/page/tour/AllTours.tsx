040d608493b8fdeec6e77ae598e936334eb4a72b
reeveboy <reeveboy10@gmail.com>
2023-07-20 21:08:06 +0530

    tours page bug fix

--- src/components/page/tour/AllTours.tsx
+++ src/components/page/tour/AllTours.tsx
@@ -38,54 +38,47 @@ function TourCard({ tour }: TourCardProps) {
         width={1000}
         height={1000}
       />
-      <div className="flex h-full flex-col justify-center p-6 md:col-span-2">
-        <div className="flex items-end justify-between">
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faStar} className="h-6 w-6 text-primary" />
-            <p className="text-md ml-1 font-light text-slate-500">
-              {tour.stars} Superb
-            </p>
-          </div>
+      <div className="flex h-full flex-col p-6 md:col-span-2 md:justify-center">
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faStar} className="h-6 w-6 text-primary" />
+          <p className="text-md ml-1 font-light text-slate-500">
+            {tour.stars} Superb
+          </p>
         </div>
         <p className="mt-4 text-3xl leading-[95%] text-slate-700">
           {tour.tourName}
         </p>
         <p className="text-xl font-light text-slate-500">{tour.location}</p>
-        {/* <p className="text-md line-clamp-2 hidden font-light leading-[120%] text-slate-500 md:line-clamp-4 md:block">
-          {tour.description}
-        </p> */}
-        <div className="mt-4 ">
-          <div className="h-[1px] bg-slate-500"></div>
-          <div className="mt-4 flex justify-center md:justify-start">
-            <div className="mr-4 flex items-center">
-              <FontAwesomeIcon
-                icon={faClock}
-                className="mr-1 h-5 w-5 text-primary"
-              />
-              <p className="text-md font-light text-slate-500">
-                {tour.days} Days
-              </p>
-            </div>
+        <div className="mt-4 h-[1px] bg-slate-500"></div>
+        <div className="mt-4 flex justify-center md:justify-start">
+          <div className="mr-4 flex items-center">
+            <FontAwesomeIcon
+              icon={faClock}
+              className="mr-1 h-5 w-5 text-primary"
+            />
+            <p className="text-md font-light text-slate-500">
+              {tour.days} Days
+            </p>
+          </div>
 
-            <div className="mr-4 flex items-center">
-              <FontAwesomeIcon
-                icon={faUser}
-                className="mr-1 h-5 w-5 text-primary"
-              />
-              <p className="text-md font-light text-slate-500">
-                {tour.groupSize}
-              </p>
-            </div>
+          <div className="mr-4 flex items-center">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="mr-1 h-5 w-5 text-primary"
+            />
+            <p className="text-md font-light text-slate-500">
+              {tour.groupSize}
+            </p>
+          </div>
 
-            <div className="flex items-center">
-              <FontAwesomeIcon
-                icon={faBookmark}
-                className="mr-1 h-5 w-5 text-primary"
-              />
-              <p className="text-md font-light capitalize text-slate-500">
-                {tour.cartegory}
-              </p>
-            </div>
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              icon={faBookmark}
+              className="mr-1 h-5 w-5 text-primary"
+            />
+            <p className="text-md font-light capitalize text-slate-500">
+              {tour.cartegory}
+            </p>
           </div>
         </div>
       </div>
